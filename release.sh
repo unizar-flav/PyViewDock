98e80f5d4e1c55ae09adc1a16fa39e4128624c5b
boneta <boneta@unizar.es>
2023-02-22 13:05:29 +0100

    Improve release script

--- release.sh
+++ release.sh
@@ -2,6 +2,13 @@
 
 ## Compress files to be published with the release
 
+pyviewdock_path=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+cd $pyviewdock_path
+
+# release folder
+rm -rf "release"
+mkdir "release"
+
 # Main plug-in files
 name="PyViewDock"
 cp_contents="README.md \
@@ -10,10 +17,12 @@ for file in $cp_contents; do
     cp $file $name/
 done
 rm -rf $name/__pycache__
-zip ${name}.zip -r $name
+zip "release"/${name}.zip -r $name
 for file in $cp_contents; do
     rm $name/$file
 done
 
 # Examples
-zip examples.zip -r examples
+zip "release"/examples.zip -r examples
+
+cd -
